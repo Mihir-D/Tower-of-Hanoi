d81dbedfd5d84d181e6b725906d6cb087d617705
Mihir-D <mdeshingkar0@gmail.com>
2022-06-04 13:39:51 -0400

    layed out initial logic

---
+++ tower_of_hanoi.py
@@ -0,0 +1,22 @@
+
+
+def solveTowerOfHanoi(n, A, B, C):
+    if n == 0:
+        return
+
+    # move n-1 top elements to C
+    solveTowerOfHanoi(n-1, A, B, C)
+    # move stack A top element to stack B
+    # move n-1 elements which are on C to B
+    solveTowerOfHanoi(n-1, C, B, A)
+
+if __name__ == __main__:
+    # read input
+    # A = stack of disks. add n, n-1, ... 1 to stack
+    # B = empty destination stack
+    # C = empty auxiliary stack
+    solveTowerOfHanoi(n, A, B, C)
+
+    print(A)
+    print(B)
+    print(C)
