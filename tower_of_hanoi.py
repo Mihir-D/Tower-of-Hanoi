5ddf4bd639877952347874e63f95943cc5beb4a5
Mihir-D <mdeshingkar0@gmail.com>
2022-06-04 14:05:27 -0400

    all logic implemented

--- tower_of_hanoi.py
+++ tower_of_hanoi.py
@@ -1,12 +1,20 @@
-
+"""
+Problem definition:
+Move all disks from tower A to tower B, tower C being auxiliary.
+Rule: Cannot place larger disk on top of smaller disk
+"""
+def move(tower_1, tower_2):
+    tower_1_element = tower_1.pop()
+    tower_2.append(tower_1_element)
 
 def solveTowerOfHanoi(n, A, B, C):
     if n == 0:
         return
 
     # move n-1 top elements to C
-    solveTowerOfHanoi(n-1, A, B, C)
+    solveTowerOfHanoi(n-1, A, C, B)
     # move stack A top element to stack B
+    move(A, B)
     # move n-1 elements which are on C to B
     solveTowerOfHanoi(n-1, C, B, A)
 
@@ -14,12 +22,16 @@ if __name__ == "__main__":
     # read input
     print("Enter number of disks: ")
     total_disks = int(input())
-    print("total disks entered by user = ", total_disks)
     # A = stack of disks. add n, n-1, ... 1 to stack
-    # B = empty destination stack
-    # C = empty auxiliary stack
-    # solveTowerOfHanoi(n, A, B, C)
+    A = []
+    for i in range(total_disks):
+        A.append(total_disks - i)
 
-    # print(A)
-    # print(B)
-    # print(C)
+    B = [] # destination tower
+    C = [] # empty auxiliary tower
+    solveTowerOfHanoi(total_disks, A, B, C)
+
+    print("final state of towers!")
+    print("A: ", A)
+    print("B: ", B)
+    print("C: ", C)
